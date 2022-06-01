b4e834ab53c543a333887c2af3e54cb838be84fd
Jean C. Contreras <jean.cntrs@gmail.com>
2022-06-01 16:20:02 -0400

    Refactored code in packages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/JeanCntrs/bookings/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Printf("Starting application on port %s\n", portNumber)
 
