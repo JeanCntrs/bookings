c046d62edb24032a1859db9f68d0599cec239e49
Jean C. Contreras <jean.cntrs@gmail.com>
2022-06-01 09:26:27 -0400

    Served HTML templates

--- main.go
+++ main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.html")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the about page")
+	renderTemplate(w, "about.html")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, err := template.ParseFiles("./templates/" + tmpl)
+	if err != nil {
+		fmt.Println("error parsing file template:", err)
+		return
+	}
+
+	err = parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error executing template:", err)
+		return
+	}
 }
 
 func main() {
