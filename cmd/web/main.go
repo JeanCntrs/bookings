1bee5c6bd997627875d67744874976797a473c11
Jean C. Contreras <jean.cntrs@gmail.com>
2022-06-03 15:55:25 -0400

    Added repo config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -23,6 +23,9 @@ func main() {
 	app.TemplateCache = tc
 	app.UseCache = true
 
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
 	render.NewTemplates(&app)
 
 	http.HandleFunc("/", handlers.Repo.Home)
