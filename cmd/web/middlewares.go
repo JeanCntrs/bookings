ffa0a419beefe785c8af1685bcffade6e81f9cc8
Jean C. Contreras <jean.cntrs@gmail.com>
2022-06-06 22:12:16 -0400

    Added and configured session

--- cmd/web/middlewares.go
+++ cmd/web/middlewares.go
@@ -6,22 +6,21 @@ import (
 	"github.com/justinas/nosurf"
 )
 
-// func writeToConsole(next http.Handler) http.Handler {
-// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		fmt.Println("Hit the page")
-// 		next.ServeHTTP(w, r)
-// 	})
-// }
-
+// noSurf adds CSRF protection to all POST requests
 func noSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
-		Secure:   false,
+		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 
 	return csrfHandler
+}
+
+// sessionLoad loads and saves the session on every request
+func sessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
