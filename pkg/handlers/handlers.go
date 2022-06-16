dfc1cd0b1927dcc6e511c4592220437145745a93
Jean C. Contreras <jean.cntrs@gmail.com>
2022-06-15 20:40:58 -0400

    Added handler that responds json

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/JeanCntrs/bookings/pkg/config"
@@ -72,6 +74,27 @@ func (rp *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	end := r.Form.Get("end")
 
 	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s", start, end)))
+}
+
+// AvailabilityJSON handles request for availability and send JSON response
+func (rp *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	type jsonResponse struct {
+		OK      bool   `json:"ok"`
+		Message string `json:"message"`
+	}
+
+	resp := jsonResponse{
+		OK:      true,
+		Message: "Available!",
+	}
+
+	out, err := json.Marshal(resp)
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 // Contact renders the contact page
