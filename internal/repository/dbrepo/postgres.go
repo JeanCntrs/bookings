9d0f60bda29383cfc9fd42b0e6f25ffa58662eed
Jean C. Contreras <jean.cntrs@gmail.com>
2022-07-19 11:02:18 -0400

    Added insert room restriction function

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -12,27 +12,52 @@ func (pdb *postgresDBRepo) AllUsers() bool {
 }
 
 // InsertReservation inserts a reservation into the database
-func (pdb *postgresDBRepo) InsertReservation(res models.Reservation) error {
+func (pdb *postgresDBRepo) InsertReservation(r models.Reservation) (int, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
+	var newID int
 	stmt := `insert into reservations (first_name, last_name, email, phone, start_date,
 			end_date, room_id, created_at, updated_at)
-			values($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+			values($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
 
-	result, err := pdb.DB.ExecContext(ctx, stmt,
-		res.FirstName,
-		res.LastName,
-		res.Email,
-		res.Phone,
-		res.StartDate,
-		res.EndDate,
-		res.RoomID,
+	err := pdb.DB.QueryRowContext(ctx, stmt,
+		r.FirstName,
+		r.LastName,
+		r.Email,
+		r.Phone,
+		r.StartDate,
+		r.EndDate,
+		r.RoomID,
 		time.Now(),
 		time.Now(),
+	).Scan(&newID)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return newID, nil
+}
+
+// InsertRoomRestriction inserts a room restriction into the database
+func (pdb *postgresDBRepo) InsertRoomRestriction(rr models.RoomRestriction) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `insert into room_restrictions (start_date, end_date, room_id, reservation_id,
+			created_at, updated_at, restriction_id)
+			values($1, $2, $3, $4, $5, $6, $7)`
+
+	_, err := pdb.DB.ExecContext(ctx, stmt,
+		rr.StartDate,
+		rr.EndDate,
+		rr.RoomID,
+		rr.ReservationID,
+		time.Now(),
+		time.Now(),
+		rr.RestrictionID,
 	)
-
-	println("result", result)
 
 	if err != nil {
 		return err
--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,5 +5,6 @@ import "github.com/JeanCntrs/bookings/internal/models"
 type DatabaseRepo interface {
 	AllUsers() bool
 
-	InsertReservation(res models.Reservation) error
+	InsertReservation(r models.Reservation) (int, error)
+	InsertRoomRestriction(rr models.RoomRestriction) error
 }
