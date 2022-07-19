fcc5265f50f484bde6a80b0199cff599c784a6d6
Jean C. Contreras <jean.cntrs@gmail.com>
2022-07-19 12:17:28 -0400

    Added search availability by dates function

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,10 +1,15 @@
 package repository
 
-import "github.com/JeanCntrs/bookings/internal/models"
+import (
+	"time"
+
+	"github.com/JeanCntrs/bookings/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(r models.Reservation) (int, error)
 	InsertRoomRestriction(rr models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
